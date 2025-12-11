f645973944e3c90cc2891f002fe47ebf8b6181eb
Racek256 <radekcerny256@gmail.com>
2025-12-11 23:28:45 +0100

    Few improvements

--- src/frontend/src/components/CodeExecutionScreen.jsx
+++ src/frontend/src/components/CodeExecutionScreen.jsx
@@ -1,15 +1,21 @@
 import Typewriter from 'typewriter-effect';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const options = {
 	delay:10,
 }
 export default function Terminal({hide}){
 	const [displayOutput, setDisplayOutput] = useState(false);
+	const [displayTerm, setDisplayTerm] = useState(false);
+	useEffect(()=>{
+		setTimeout(()=>{
+
+		setDisplayTerm(true)
+		},1)
+	},[])
 	return(
-		<div className="w-screen top-0 left-0 h-screen fixed backdrop-blur-xs  z-999">
 			
-			<div className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 rounded-xl h-1/2 flex border border-runner-border bg-runner-bg px-4 py-2 flex-col flex"	>
+			<div className={` absolute bottom-0 left-0 w-full z-9999  ${displayTerm ? "h-1/3 py-2": "h-0 py-0"} ease-in-out overflow-hidden transition-200 transition-all flex border-t border-border bg-runner-bg px-4  flex-col flex`}	>
 		<Typewriter className="text-3xl my-2 text-white" wrapperClassName="text-white text-3xl" 
 
 		options={options}
@@ -45,8 +51,8 @@ export default function Terminal({hide}){
 
 		<p className={`bg-runner-output transition-400 ${!displayOutput ? "opacity-0" : "opacity-100"}  transition-all rounded-xl p-4 text-runner-text`}>output: 69</p>
 
-
-		<button className='bg-button absolute right-5 bottom-2 w-max p-4 px-6 flex items-center  transition  h-12 rounded-md hover:bg-button-hover cursor-pointer text-xl' onClick={()=>{hide()}}>Close</button>
+		<button className='bg-button absolute right-5 top-5  w-max p-4 m-2  flex items-center  transition   rounded-xl hover:bg-button-hover cursor-pointer text-xl' onClick={()=>{ setDisplayTerm(false); 
+			setTimeout(()=>{hide()},200)}}>Close</button>
 		</div>
-		</div>)
+	)
 }
