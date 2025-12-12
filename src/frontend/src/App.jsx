725049b88f0dbff2a697eef0f1d3b9d9b2e293ae
Racek256 <radekcerny256@gmail.com>
2025-12-12 20:49:39 +0100

    reformated everything with prettier

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import { CookiesProvider } from 'react-cookie'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import { CookiesProvider } from "react-cookie";
 function App() {
   return (
-	  <CookiesProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-	  </CookiesProvider>
-  )
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  );
 }
 
-export default App
+export default App;
--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,23 +1,20 @@
-import User from '../assets/user_icon.svg'
-export default function Navbar(){
-	return(
-		<div className="h-20  border-b  border-border w-full flex bg-bg">
-		<div className="h-full w-5/8 border-r border-border">
-		<h1 className="text-xl text-text-light p-2 pb-0">Vyřeš úlohu</h1>
-		<p className="p-2 text-lg text-text-light">Tady bude úloha</p>
-			
-		</div>
-		<div className="flex">
-		<img  className="rounded-full p-2" src={User}/>	
-		<div>
-			<h1 className="text-white text-xl p-2 pb-0">Zaměstnavatel</h1>
-			<div className="rounded-xl border-3 border-black h-6 w-64 m-2 overflow-hidden relative"><div className="w-1/2 bg-gradient-to-r from-ctp-green-600 to-ctp-red-900 h-full "></div>
-
-		</div>
-		</div>
-		</div>
-			
-		</div>
-	)
-
+import User from "../assets/user_icon.svg";
+export default function Navbar() {
+  return (
+    <div className="h-20  border-b  border-border w-full flex bg-bg">
+      <div className="h-full w-5/8 border-r border-border">
+        <h1 className="text-xl text-text-light p-2 pb-0">Vyřeš úlohu</h1>
+        <p className="p-2 text-lg text-text-light">Tady bude úloha</p>
+      </div>
+      <div className="flex">
+        <img className="rounded-full p-2" src={User} />
+        <div>
+          <h1 className="text-white text-xl p-2 pb-0">Zaměstnavatel</h1>
+          <div className="rounded-xl border-3 border-black h-6 w-64 m-2 overflow-hidden relative">
+            <div className="w-1/2 bg-gradient-to-r from-ctp-green-600 to-ctp-red-900 h-full "></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
