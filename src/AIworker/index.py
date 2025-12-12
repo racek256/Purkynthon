95a26f191ba01b04a567d212742e4aab815c7b59
itsfimes <fimesik@gmail.com>
2025-12-12 15:02:48 +0100

    the file that shall be forgotten.

--- src/AIworker/index.py
+++ src/AIworker/index.py
@@ -1,24 +1,25 @@
-from ollama import chat
-from ollama import Client
-from ollama import ChatResponse
-from flask_cors import CORS
-
-client = Client(
-        host='http://192.168.0.125:11434')
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/chat', methods=['POST'])
-def chatwithAI():
-    data = request.get_json()
-    history = data.get("history")
-    
-    response: ChatResponse = client.chat(model='gemma3:1b-it-qat', messages=history)
-    return jsonify(message=response.message.content)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=5068)  # Run the Flask server
-
+# forget the existence of this file. it is cursed.
+# from ollama import chat
+# from ollama import Client
+# from ollama import ChatResponse
+# from flask_cors import CORS
+#
+# client = Client(
+#         host='http://192.168.0.125:11434')
+# from flask import Flask, jsonify, request
+#
+# app = Flask(__name__)
+# CORS(app)
+#
+# @app.route('/api/chat', methods=['POST'])
+# def chatwithAI():
+#     data = request.get_json()
+#     history = data.get("history")
+#
+#     response: ChatResponse = client.chat(model='gemma3:1b-it-qat', messages=history)
+#     return jsonify(message=response.message.content)
+#
+#
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", debug=True, port=5068)  # Run the Flask server
+#
