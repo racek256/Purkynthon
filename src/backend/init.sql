3ed262090dbbbe4c823c2e7195fda2c6dc922572
Racek256 <radekcerny256@gmail.com>
2026-01-14 14:33:43 +0100

    few changes idk what stuff may break

--- src/backend/init.sql
+++ src/backend/init.sql
@@ -5,6 +5,13 @@ CREATE TABLE users (
 	score INTEGER DEFAULT 0,
 	level INTEGER DEFAULT 1
 );
+CREATE TABLE finished_lessons (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER NOT NULL,
+	earned_score INTEGER, 
+	time_to_finish INTEGER NOT NULL,
+	lesson_id INTEGER NOT NULL
+);
 
 
 INSERT INTO users (username, password) VALUES ('admin', '$2b$12$Bf7RU727OpvGqrz2YVoUFeySGw3pl9rlnirzekA6eqAziuw2ztG8y');
